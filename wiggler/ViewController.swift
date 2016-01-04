0bef18dee3c1bd149341868b4e8388b7b705ca04
Luis Carlos Rosa <loi986@gmail.com>
2016-01-04 11:02:22 +0000

    Initial Commit

---
+++ wiggler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wiggler
+//
+//  Created by Luis Carlos Rosa on 04/01/16.
+//  Copyright © 2016 Wiggler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
